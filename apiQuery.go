15473fb3da62b8b8b70dffa2b98ba79e1da819f9
Alex Govan <alexandertgovan@aol.com>
2018-09-15 15:02:00 -0700

    It runs! Figured out how to extrapolate more information from bad HTTP requests, making sure structs are formatted exactly right.

--- apiQuery.go
+++ apiQuery.go
@@ -7,11 +7,21 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
-// Apiconfig - Importing access token from config.json
+// Query - Used to send query information to graphQL
+// Variables is declared as empty rather than as a string of open/close brackets
+// GraphQL expects brackets and not a string, otherwise will throw Bad Json request
+type Query struct {
+	Query     string   `json:"query"`
+	Variables struct{} `json:"variables"`
+}
+
+// Apiconfig - Importing configs from config.json
 type Apiconfig struct {
-	Token string
+	Token   string // Access token
+	Timeout int    // HTTP timeout
 }
 
 func main() {
@@ -26,31 +36,36 @@ func main() {
 	json.Unmarshal(byteValue, &config)
 
 	client := &http.Client{}
+	client.Timeout = time.Second * time.Duration(config.Timeout)
 
 	query := `{
-		device(id: 212014918250766) {
-		  groupId
-		  product {
-			shortName
-		  }
+		group(id: 3991) {
+			name
 		}
-	  }`
+	}`
 
-	postdata := []byte(`{"query":` + query + `, "variables": {}}`)
-	pd, _ := json.Marshal(string(postdata))
-	fmt.Println(string(pd))
+	q := Query{
+		Query: query,
+	}
+	b, err := json.Marshal(q)
 
 	// Generate the API query
 	url := "https://api.samsara.com/v1/admin/graphql"
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(pd))
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(b))
 	if err != nil {
 		fmt.Printf("Error generating request: %s", err)
 	}
-
 	req.Header.Add("X-Access-Token", config.Token)
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Printf("Error getting response: %s", err)
 	}
-	fmt.Println(resp)
+	// Check if we get any page errors, this is not caught by err
+	if resp.StatusCode != 200 {
+		b, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println(string(b))
+		return
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 }
